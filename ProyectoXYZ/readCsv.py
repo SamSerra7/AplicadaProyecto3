16e2654aee890950dddf7d55bf74c2aaa30fa221
nasg2013 <62192753+nasg2013@users.noreply.github.com>
2020-10-12 17:51:37 -0600

    Update readCsv.py

--- ProyectoXYZ/readCsv.py
+++ ProyectoXYZ/readCsv.py
@@ -4,15 +4,17 @@ import operator
 
 #se lee el archivo deseado
 with open('MOCK_DATA.csv') as csvarchivo:
+    #crea una lista con las entradas
     entrada = csv.reader(csvarchivo)
-    for reg in entrada:
-        #Se imprime todo el registro cada línea se muestra como una lista de campos
+      for reg in entrada:
+        # Se imprime todo el registro cada línea se muestra como una lista de campos
+        # se extraen las columnas
+        id, first_name, last_name, email, gender, ip_address = reg
+        # se imprimen campos deseados
+        print(id, first_name, last_name, gender)
+        # imprime toda la fila
+        # print(reg)
         #print(reg)
-
-        #se extraen las columnas
-        id,first_name,last_name,email,gender,ip_address = next(entrada)  # Leer campos
-        #se imprimen campos deseados
-        print(id,first_name)  # Mostrar campos
 
 #referencia
 #https://python-para-impacientes.blogspot.com/2015/05/operaciones-con-archivos-csv.html
